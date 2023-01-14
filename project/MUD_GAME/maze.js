a82c0b5afa31fcc1021930865ba8506b2d8b740e
DESKTOP-FT8VBUM\user <espoir3045@gmail.com>
2023-01-14 13:54:11 +0900

    update file

--- project/MUD_GAME/maze.js
+++ project/MUD_GAME/maze.js
@@ -1,8 +1,7 @@
-
 // 1) width, height가 둘 다 홀수인 2차원 배열을 준비한다.
-let board=[];
-let width=35;
-let height=35;
+let board = [];
+let width = 35;
+let height = 35;
 class Stack {
     constructor() {
         this._arr = [];
@@ -20,6 +19,8 @@ class Stack {
         return this._arr[this._arr.length-1];
     }
 }
+let map='', maze = [], subMap2='';
+let fs = require('fs');
 let createMaze=()=>{
     // 2) 모든 셀을 “방문하지 않음” 상태로 만든다.
     for(let y = 0; y < height; y += 1){
@@ -118,36 +119,147 @@ let createMaze=()=>{
     //(x=width-2, y=height-1) 좌표를 방문함 상태로 만들고 출구로 사용한다.
     board[width-1][width-2].visit = true;
     board[width-1][width-2].output = '출구';
-    let cell,maze=[],body='';
-
     let draw=()=>{
-        cell=Buffer.alloc(3);
         for( let y = 0; y < width; y += 1 ){
-            maze.push([]);
-            for(let x=0; x<height; x+=1){
-                maze[y].push([cell]);
+             maze.push([]);
+            for(let x=0; x < height; x+=1){
                 if( board[y][x].visit === true ){
                     board[y][x].player === false;
                     if( board[y][x].output === '출구' ){
-                        maze[y][x]=Buffer.from('&');
-                    }else if( board[y][x].input==='입구' ){
-                        maze[y][x]=Buffer.from('&');
+                        maze[y][x]='&';
+                    }else if( board[y][x].input === '입구' ){
+                        maze[y][x]='&';
                     }else if( y === 1 && x === 1 ){
-                        maze[1][1]=Buffer.from('@');
+                        maze[1][1]='@';
                     }else{
-                        maze[y][x]=Buffer.from('.');
+                        maze[y][x]='.';
                     }
                 }
                 else if(board[y][x].visit === false){
-                    maze[y][x]=Buffer.from('#');
+                    maze[y][x]='#';
                 }
             }
             // 정규표현식 모든 , 를 찾아서 공백으로 바꾸기
-            body+=(maze[y].toString()).replace(/,/g,Buffer.from(' '))+`\n`
+            // map표시 (엔터처리 필요) 36바이트 (\n 추가로인한)
+            map+=maze[y].toString().replace(/,/g,'')+`\n`;
         }
-        console.log(body)
+        map=Buffer.from(map);
+        console.log(map.toString());
     }
     draw();
 }
+// 첫 미로 생성
 createMaze();
+
+// 입력받을 명령어들
+let end = Buffer.from('end');//끝
+let up = Buffer.from('w');   //북
+let down = Buffer.from('s'); //남
+let right = Buffer.from('d');//동
+let left = Buffer.from('a'); //서
+// 요소들
+let player = '@';
+let road = '.';
+let door = '&';
+let wall = '#';
+let playerX = 1, playerY = 1, currPos, subMap = [];
+// 전체맵 출력 process.stdout.write(map.toString())
+//보여질 맵 7x7 출력 함수
+let outputMap=function(){
+    for(let y = 0; y<width;y+=1){
+        for(let x=0;x<maze[y].length;x+=1){
+            if(maze[y][x]==='@'){
+                playerY=y;
+                playerX=x;
+            } 
+        }
+    }
+    // 초기화
+    subMap2 = '';
+    for(let i=0; i<=6; i+=1){
+         // 가장 끝부분(모서리)
+        if(playerY <= 3 && playerX <= 3){
+            subMap[i+3] = maze[i].slice(0, 7);
+        }else if(playerY > 31 && playerX > 31){
+            subMap[i+3] = maze[height-(6+i)].slice(width-7, width+1);
+        }else if(playerY <= 3 && playerX > 31){
+            subMap[i+3] = maze[i].slice(width-7, width+1);
+        }else if(playerY > 31 && playerX <= 3){
+            subMap[i+3] = maze[height-(6+i)].slice(0, 7);
+        }
+        //가장자리 
+        else if(playerY > 3 && playerX <= 3 && playerY <= 31){
+            subMap[i+3] = maze[playerY+(-3+i)].slice(0,7);
+        }
+        else if(playerY <= 3 && playerX > 3 && playerX <= 31){
+            subMap[i+3] = maze[i].slice(playerX-3, playerX+4);
+        }
+        else if(playerY > 31 && playerX <= 31 && playerX > 3){
+            subMap[i+3] = maze[height-(6+i)].slice(playerX-3, playerX+4);
+        }
+        else if(playerY <= 31 && playerX > 31 && playerY > 3){
+            subMap[i+3] = maze[playerY+(-3+i)].slice(width-7, width+1);
+        }
+        // //중앙
+        else if(playerY > 3 && playerX > 3 && playerY <= 31 && playerX <= 31){
+            subMap[i+3] = maze[playerY+(-3+i)].slice(playerX-3, playerX+4);
+        }
+        // 조립
+        subMap2+=subMap[i+3].toString().replace(/,/g,'')+`\n`;
+    }
+    //출력
+    console.log(Buffer.from(subMap2).toString().trim());
+}
+outputMap();
+
+process.stdin.on('data',function(buf){
+    if((buf.indexOf(end.toString()) !== -1) && (buf.indexOf(Buffer.from([10])) !== -1) ){
+        process.exit();
+    }else if((buf.indexOf(Buffer.from('load')) !== -1) && (buf.indexOf(Buffer.from([10])) !== -1) ){
+        map = fs.readFileSync( './save1.txt' );
+        outputMap();
+    }else if((buf.indexOf(Buffer.from('save')) !== -1) && (buf.indexOf(Buffer.from([10])) !== -1) ){
+        fs.writeFileSync('./save1.txt', map);
+    }else if(((buf.indexOf(up.toString()) !== -1) ||
+            (buf.indexOf(down.toString()) !== -1) ||
+            (buf.indexOf(right.toString()) !== -1) ||
+            (buf.indexOf(left.toString()) !== -1)) && 
+            ((buf.indexOf(Buffer.from([10])) !== -1))){
+        if(buf.indexOf(up) !== -1){
+            if((maze[playerY-1][playerX] === road) && playerY-1 > 0){
+                if(playerY <= 35) { map[(playerY *36) +playerX] = 46 }
+                maze[playerY][playerX] = road;
+                maze[playerY-1][playerX] = player;
+                map[((playerY-1)*36)+playerX] = 64
+            }
+        }else if(buf.indexOf(down) !== -1){
+            if((maze[playerY+1][playerX] === '.') && playerY+1 <= 34){
+                if(playerY >= 1) { map[(playerY * 36) + playerX] = 46 }
+                maze[playerY][playerX] = road;
+                maze[playerY+1][playerX] = player;
+                map[((playerY+1)*36)+playerX] = 64
+            }else if((maze[playerY+1][playerX]) === door && playerY+1 <= 35){
+                process.stdout.write(Buffer.from('Clear'));
+                process.exit();
+            }
+        }else if(buf.indexOf(right) !== -1){
+            if((maze[playerY][playerX + 1] === road) && playerX + 1 <= 34){
+                if(playerX >= 1) { map[(playerY * 36) + playerX] = 46 }
+                maze[playerY][playerX] = road;
+                maze[playerY][playerX+1] = player;
+                map[(playerY * 36) + playerX+1] = 64
+            }
+        }else if((buf.indexOf(left) !== -1)){
+            if((maze[playerY][playerX-1] === road)&& playerX-1 >= 1){
+                if(playerX <= 35) { map[(playerY * 36) + playerX] = 46 }
+                maze[playerY][playerX] = road;
+                maze[playerY][playerX-1] = player;
+                map[(playerY*36)+playerX-1] = 64;
+            }
+        }
+        outputMap();
+    }
+    console.log(playerX,playerY)
+});
+
 
